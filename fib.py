d5f88c8f66fa37c50fcb70952a8fe5f1987c5959
Giacomo Sfratato <xbit18@hotmail.it>
2024-02-16 11:35:28 +0100

    New Repo

---
+++ fib.py
@@ -0,0 +1,22 @@
+import sys
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+print(f"NoGil: {getattr(sys.flags, 'nogil', False)}")
+
+def fib(n):
+    if n < 2: return 1
+    return fib(n-1) + fib(n-2)
+
+threads = 8
+if len(sys.argv) > 1:
+    threads = int(sys.argv[1])
+    
+start_time = time.time()
+with ThreadPoolExecutor(max_workers=threads) as executor:
+    for _ in range(threads):
+        executor.submit(lambda: fib(34))
+
+total_time = round(time.time()-start_time, 3)
+print(total_time)
