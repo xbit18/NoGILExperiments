db43f8fd0c62b852e6025af17417ac9c03ea9aea
Giacomo Sfratato <xbit18@hotmail.it>
2024-04-04 13:13:35 +0200

    memory

--- script.py
+++ script.py
@@ -41,14 +41,20 @@ def check_versions(versions):
 
 
 # Test single thread
-def single_thread(versions):
+def single_thread(versions, memory=False):
     subprocess.run("pyperf system tune", shell=True)
 
     for version, done in versions.items():
         if done[0]:
             continue
+
+        command = f"pyperformance run --python={os.environ['HOME']}/.pyenv/versions/{version}/bin/python"
+        if memory:
+            command += " -m"
+        command += f" -o ./pyperf_res/{version}.json"
+
         subprocess.run(
-            f"pyperformance run --python={os.environ['HOME']}/.pyenv/versions/{version}/bin/python -o ./pyperf_res/{version}.json",
+            command,
             shell=True)
         versions[version][0] = True
         with open("./versions.json", "w") as f:
@@ -56,6 +62,7 @@ def single_thread(versions):
             f.write(versions_json)
 
     subprocess.run("pyperf system reset", shell=True)
+
 
 
 # Test multi thread
@@ -194,6 +201,84 @@ def analyse_single_thread():
     plt.savefig(f"./images/confronto_single_thread.png", bbox_inches='tight', transparent=False, pad_inches=0.1)
 
 
+def analyse_memory_single_thread():
+    path = './pyperf_res/memory'
+    files_to_process = []
+    for file_name in os.listdir(path):
+        if file_name.endswith('.json'):
+            files_to_process.append(file_name)
+
+    processed_files = {}
+    for file in files_to_process:
+        f = open(path + file)
+        data = json.load(f)
+        benchmarks = {}
+        for d in data['benchmarks'][1:]:
+            benchmarks[d['metadata']['name']] = d['runs'][1:]
+        processed_files[f"{file.replace('.json', '')}_processed.json"] = benchmarks
+
+    columns = {
+        'py310_processed.json': '3.10.13',
+        'py311_processed.json': '3.11.8',
+        'py312_processed.json': '3.12.2',
+        'py3120_processed.json': '3.12.0',
+        'py39_processed.json': '3.9.18',
+        'py3910_processed.json': '3.9.10',
+        'pynogil_processed.json': 'nogil-3.9.10'
+    }
+
+    # Get complete list of benchmarks
+    benchmarks = []
+    for file in sorted(processed_files):
+        benchmarks.extend(processed_files[file].keys())
+
+    benchmarks = sorted(list(set(benchmarks)))
+    df = {}
+    df['Benchmarks'] = benchmarks
+    for file in sorted(processed_files):
+        all_mems = []
+        data = processed_files[file]
+        for key in benchmarks:
+            if data.get(key):
+                bench = data[key]
+                mems = []
+                for run in bench:
+                    vals = run['values']
+                    mems.extend(vals)
+                # print(times)
+                avg_mem = np.average(mems)
+                avg_mem = avg_mem/1024/1024
+                all_mems.append(round(avg_mem, 1))
+            else:
+                all_mems.append(np.nan)
+        df[columns[file].replace('.json', '')] = all_mems
+        
+    mems_df = pd.DataFrame(df)
+    columns = ['Benchmarks', '3.9.10', 'nogil-3.9.10', '3.9.18', '3.10.13', '3.11.8', '3.12.0', '3.12.2']
+    mems_df = mems_df[columns]
+    mems_df_notnull = mems_df.dropna()
+    mems_df_notnull.reset_index(inplace=True, drop=True)
+    mems_df = mems_df_notnull
+
+    mems = []
+    for col in mems_df.columns[1:]:
+        avg_mem = np.average(mems_df[col])
+        mems.append(avg_mem)
+
+    plt.figure(figsize=(10, 7))
+    labels = list(mems_df.columns)[1:]
+
+    colors = ["#ff1500", "#ff9602", "#f5cc02", "#00d200", "#00c3ff", "#0022ff", "#b700ff"]
+    colors.reverse()
+    for i in range(len(labels)):
+        plt.bar(i, mems[i], color=colors.pop())
+    plt.xlabel("Python Versions")
+    plt.ylabel("Avg Execution Time")
+    plt.legend(labels)
+    ticks = [i for i in range(len(mems))]
+    plt.xticks(ticks, labels=labels)
+    plt.savefig(f"./images/confronto_single_thread_memoria.png", bbox_inches='tight', transparent=False, pad_inches=0.1)
+
 def analyse_multi_thread():
     arr = np.genfromtxt('times.csv',delimiter=',',dtype=str)
     df = pd.DataFrame(arr.T)
@@ -247,8 +332,10 @@ def main():
     check_versions(versions)
 
     single_thread(versions)
+    single_thread(versions, memory=True)
     multi_thread(versions)
     analyse_single_thread()
+    analyse_memory_single_thread()
     analyse_multi_thread()
 
 if __name__ == '__main__':
